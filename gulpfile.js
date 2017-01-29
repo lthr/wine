92dae80fea2548e15e2bc6d4e9bef15b28fab48f
Mike Lothar <mikelothar@gmail.com>
2017-01-29 23:41:36 +0100

    add gulp dist task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,57 @@
 const gulp = require('gulp');
+const del = require('del');
+const concat = require('gulp-concat');
+const htmlreplace = require('gulp-html-replace');
 
-gulp.task('default', () => {
-  gulp.src('./src/**/*')
-      .pipe(gulp.dest('dist'));
+const appJs = [
+    './src/**/*.js'
+];
+
+const vendorJs = [
+  './node_modules/angular/angular.min.js',
+  './node_modules/angular-ui-router/release/angular-ui-router.min.js',
+  './node_modules/ngstorage/ngStorage.min.js'
+];
+
+const vendorCss = [
+  './node_modules/bootstrap/dist/css/bootstrap.min.css'
+];
+
+gulp.task('clean', () => {
+  return del(['./dist/*']);
+});
+
+gulp.task('dist:app:js', () => {
+  return gulp.src(appJs)
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('dist:vendor:js', () => {
+  return gulp.src(vendorJs)
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('dist:vendor:css', () => {
+  return gulp.src(vendorCss)
+      .pipe(concat('vendor.css'))
+      .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('dist:app:css', () => {
+  return gulp.src('./src/style/style.css')
+      .pipe(concat('app.css'))
+      .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('dist', ['dist:vendor:js', 'dist:vendor:css', 'dist:app:css', 'dist:app:js'], () => {
+  return gulp.src('./src/index.html')
+      .pipe(htmlreplace({
+        'vendorCss': 'css/vendor.css',
+        'vendorJs': 'js/vendor.js',
+        'appCss': 'css/app.css',
+        'appJs': 'js/app.js'
+      }))
+      .pipe(gulp.dest('./dist'));
 });

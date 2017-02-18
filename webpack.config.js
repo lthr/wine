e560253bf8ad7d54138a73f8c011d1263f2f2dec
Mike Lothar <mikelothar@gmail.com>
2017-02-18 22:56:11 +0100

    fix basehref for prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = function (env) {
-    return {
-        entry: './src/app/app.module.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: env.prod
-                ? '[name].[hash].js'
-                : '[name].bundle.js'
-        },
-        plugins: [new HtmlWebpackPlugin({template: './src/index.html'})],
-        devtool: 'eval-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, "dist"),
-            compress: true,
-            port: 9000
-        },
+  return {
+    entry: './src/app/app.module.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: env.prod
+          ? '[name].[hash].js'
+          : '[name].bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        baseHref: env.prod ? '/wine/' : '/',
+        template: './src/index.ejs',
+        filename: 'index.html'
+      })
+    ],
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000
+    },
 
-        module: {
-            loaders: [
-                {
-                    test: /\.css$/,
-                    loaders: 'style-loader!css-loader'
-                }, {
-                    test: /\.html$/,
-                    loader: 'html-loader',
-                    exclude: /node_modules/
-                }
-            ]
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: 'style-loader!css-loader'
+        }, {
+          test: /\.html$/,
+          loader: 'html-loader',
+          exclude: /node_modules/
         }
+      ]
     }
+  }
 };
 
 // https://github.com/preboot/angular-webpack/blob/master/webpack.config.js

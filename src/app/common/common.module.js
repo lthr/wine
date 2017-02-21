4c0309c6d6914edfe091073105787db9953de88c
lthr <mikelothar@gmail.com>
2017-02-21 19:10:23 +0000

    Regenerated static content for 9ac3d057a1847a07a9a18d394673caee4ce3333a

--- src/app/common/common.module.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import {ListModule} from './list/list.module';
-import {DetailsModule} from './details/details.module';
-import {AddModule} from './add/add.module';
-
-export const CommonModule = angular
-    .module('app.common', [ListModule, AddModule, DetailsModule])
-    .name;
